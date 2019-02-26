740a3350cf72adbc6ca1339d663b9286f41d084a
Piotr Ĺawniczek <lawniczek.piotr@gmail.com>
2019-02-26 17:08:24 +0100

    Changed sqlite files permissions; named containers

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 cp ./docker/php/init.sh ./app/init.sh
-# docker-compose build --no-cache
 docker-compose build --no-cache
--- docker/php/init.sh
+++ docker/php/init.sh
@@ -1,4 +1,5 @@
 composer install --prefer-dist && \
 php bin/console doctrine:schema:update --force && \
 php bin/console doctrine:fixtures:load && \
+chmod -R 777 ./data && \
 rm /app/init.sh
